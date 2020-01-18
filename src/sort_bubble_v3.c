3819fa6a8149715f6aef9b8527b585f1cae924bd
Juhani Kauppi <jkauppi@student.hive.fi>
2020-01-18 18:20:37 +0200

    Sort bubble V3 can sort five integers in 12 moves.

--- src/sort_bubble_v3.c
+++ src/sort_bubble_v3.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 11:21:02 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/01/18 17:02:51 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/01/18 18:13:29 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static void		execute_action(t_sort_result *sort_result, int *stack,
 }
 
 static int		loop_down_if_swap(t_sort_result *sort_result,
-													t_stack_index stack_index)
+													t_stack_index *stack_index)
 {
 	int				is_sorted;
 	int				*stack;
@@ -45,68 +45,64 @@ static int		loop_down_if_swap(t_sort_result *sort_result,
 	stack = sort_result->stack;
 	is_sorted = 1;
 	loop_cnt = 0;
-	while (*(stack + stack_index.top) != sort_result->max)
+	while (*(stack + stack_index->top) != sort_result->max)
 	{
 		loop_cnt++;
-		if (*(stack + stack_index.top) > *(stack + stack_index.next))
+		if (*(stack + stack_index->top) > *(stack + stack_index->next))
 		{
-			execute_action(sort_result, stack, &stack_index, "sa");
+			execute_action(sort_result, stack, stack_index, "sa");
 			is_sorted = 0;
-			execute_action(sort_result, stack, &stack_index, "ra");
+			execute_action(sort_result, stack, stack_index, "ra");
 		}
 		else
 		{
-			if (!is_sorted && *(stack + stack_index.bottom) <=
+			if (!is_sorted && *(stack + stack_index->bottom) <=
 															sort_result->median)
 				break ;
-			execute_action(sort_result, stack, &stack_index, "ra");
+			execute_action(sort_result, stack, stack_index, "ra");
 		}
 	}
 	if (loop_cnt != sort_result->stack_size - 1)
 		is_sorted = 0;
-	sort_result->top_ptr = stack_index.top;
+	sort_result->top_ptr = stack_index->top;
 	return (is_sorted);
 }
 
 static int		loop_up_if_swap(t_sort_result *sort_result,
-													t_stack_index stack_index)
+													t_stack_index *stack_index)
 {
 	int				is_sorted;
 	int				*stack;
 
 	stack = sort_result->stack;
 	is_sorted = 0;
-	while (*(stack + stack_index.top) != sort_result->max)
+	while (*(stack + stack_index->top) != sort_result->max)
 	{
-		if (*(stack + stack_index.next) < *(stack + stack_index.top))
+		if (*(stack + stack_index->next) < *(stack + stack_index->top))
 		{
-			execute_action(sort_result, stack, &stack_index, "sa");
-			execute_action(sort_result, stack, &stack_index, "rra");
+			execute_action(sort_result, stack, stack_index, "sa");
+			execute_action(sort_result, stack, stack_index, "rra");
 		}
 		else
-			execute_action(sort_result, stack, &stack_index, "rra");
+			execute_action(sort_result, stack, stack_index, "rra");
 	}
-	execute_action(sort_result, stack, &stack_index, "ra");
-	sort_result->top_ptr = stack_index.top;
+	execute_action(sort_result, stack, stack_index, "ra");
+	sort_result->top_ptr = stack_index->top;
 	return (is_sorted);
 }
 
-static int		loop_if_swap(t_sort_result *sort_result)
+static int		loop_if_swap(t_sort_result *sort_result,
+													t_stack_index *stack_index)
 {
 	int				is_sorted;
-	t_stack_index	stack_index;
 
-	stack_index.top = sort_result->top_ptr;
-	stack_index.next = stack_index.top ? stack_index.top - 1 :
-													sort_result->stack_size - 1;
-	stack_index.bottom = (stack_index.top + 1) % sort_result->stack_size;
 	is_sorted = loop_down_if_swap(sort_result, stack_index);
 	if (!is_sorted)
 	{
-		stack_index.top = sort_result->top_ptr;
-		stack_index.next = stack_index.top ? stack_index.top - 1 :
+		stack_index->top = sort_result->top_ptr;
+		stack_index->next = stack_index->top ? stack_index->top - 1 :
 													sort_result->stack_size - 1;
-		stack_index.bottom = (stack_index.top + 1) % sort_result->stack_size;
+		stack_index->bottom = (stack_index->top + 1) % sort_result->stack_size;
 		is_sorted = loop_up_if_swap(sort_result, stack_index);
 	}
 	return (is_sorted);
@@ -114,12 +110,27 @@ static int		loop_if_swap(t_sort_result *sort_result)
 
 void			bubble_sort_v3(t_sort_result *sort_result)
 {
-	int			is_sorted;
+	int				is_sorted;
+	t_stack_index	stack_index;
+	int				*stack;
 
+	stack = sort_result->stack;
 	sort_result->top_ptr = sort_result->stack_size - 1;
+	stack_index.top = sort_result->top_ptr;
+	stack_index.next = stack_index.top ? stack_index.top - 1 :
+													sort_result->stack_size - 1;
+	stack_index.bottom = (stack_index.top + 1) % sort_result->stack_size;
+	if (*(stack + stack_index.top) > sort_result->median &&
+							*(stack + stack_index.next) > sort_result->median)
+	{
+		if (*(stack + stack_index.top) > *(stack + stack_index.next))
+			execute_action(sort_result, stack, &stack_index, "sa");
+		execute_action(sort_result, stack, &stack_index, "ra");
+		execute_action(sort_result, stack, &stack_index, "ra");
+	}
 	is_sorted = 0;
 	while (!is_sorted)
-		is_sorted = loop_if_swap(sort_result);
+		is_sorted = loop_if_swap(sort_result, &stack_index);
 	while (sort_result->stack[sort_result->top_ptr] != sort_result->min)
 	{
 		sort_result->top_ptr = ++sort_result->top_ptr % sort_result->stack_size;
